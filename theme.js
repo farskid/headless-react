632d3201a552d5bbf993935553144dd8943226e8
Farzad YZ <farskid@gmail.com>
2018-09-13 15:57:10 +0300

    :fire:

--- theme.js
+++ theme.js
@@ -1,12 +1,3 @@
-import theme from 'mdx-deck/themes'
+import {hack} from 'mdx-deck/themes'
 
-export default {
-  ...theme,
-
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
-}
+export default hack;
